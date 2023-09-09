c6c8db97b50d361b9bebd8e5403b8db43a08953e
HyuDev7 <hyu.df7@gmail.com>
2023-09-09 18:29:04 +0900

    wrote briefing texts

--- src/app/briefing/page.tsx
+++ src/app/briefing/page.tsx
@@ -1,7 +1,16 @@
 import Link from "next/link";
 import { insertIDs } from "@/lib/mongodb";
 import SplitText from "@/components/SplitText";
-import { T_text1, T_text2, T_text3, T_text4 } from "@/lib/textlist";
+import {
+  B_text1,
+  B_text2,
+  B_text3,
+  B_text4,
+  B_text5,
+  B_text6,
+  B_text7,
+} from "@/lib/textlist";
+import FlowOfPage from "@/components/FlowOfPage";
 
 export default function Page() {
   // insertIDs()
@@ -11,16 +20,26 @@ export default function Page() {
       <div className="container mx-auto">
         <h1 className="my-3">実験を始めるにあたっての説明</h1>
         <div className="textStyle">
-          <SplitText text={T_text1} />
+          <SplitText text={B_text1} />
         </div>
         <div className="textStyle">
-          <SplitText text={T_text2} />
+          <SplitText text={B_text2} />
         </div>
         <div className="textStyle">
-          <SplitText text={T_text3} />
+          <SplitText text={B_text3} />
         </div>
         <div className="textStyle">
-          <SplitText text={T_text4} />
+          <SplitText text={B_text4} />
+        </div>
+        <div className="textStyle">
+          <SplitText text={B_text5} />
+        </div>
+        <div className="textStyle mb-0">
+          <SplitText text={B_text6} />
+        </div>
+        <FlowOfPage />
+        <div className="textStyle">
+          <SplitText text={B_text7} />
         </div>
         <Link className="inputStyle" href="/auth">
           実験のログイン
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import Link from "next/link";
 import { insertIDs } from "@/lib/mongodb";
-import SplitText from "@/components/SplitText";
-import { T_text1, T_text2, T_text3, T_text4 } from "@/lib/textlist";
 
 export default function Page() {
   // insertIDs()
@@ -11,10 +9,10 @@ export default function Page() {
       <div className="container mx-auto">
         <h1 className="my-3">経済学研究に用いる分配に関するアンケート調査</h1>
         <div className="textStyle">
-          この度は実験へ参加してくださり、ありがとうございます。
-          本実験は工学院大学の研究室が実施する経済学研究に関わるアンケート調査です。
-          実験の概要とその流れについては「本実験の概要と流れについて」というページで説明しております。
-          下のボタンを押してページへお進みください。
+          この度は実験へ参加してくださり、ありがとうございます。¥n
+          本実験は工学院大学の研究室が実施する経済学研究に関わるアンケート調査です。¥n
+          実験の概要とその流れについては「本実験の概要と流れについて」というページで説明しております。¥n
+          下のボタンを押してページへお進みください。¥n
         </div>
         
         <Link className="inputStyle" href="/briefing">
