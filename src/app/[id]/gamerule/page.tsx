632d9a2f23ff75067ff7691a789eeca2932f6e7a
HyuDev7 <hyu.df7@gmail.com>
2023-11-11 09:38:13 +0900

    added text to gamerule

--- src/app/[id]/gamerule/page.tsx
+++ src/app/[id]/gamerule/page.tsx
@@ -1,11 +1,8 @@
-import SplitText from "@/components/SplitText";
-import { G_text1, G_text2, G_text3 } from "@/lib/textlist";
 import { validateSessionID } from "@/lib/validateSessionId";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
 export default async function Home({ params }: { params: { id: string } }) {
-
   const sessionID = params.id;
   const validationResult = await validateSessionID(sessionID);
   if (!validationResult) {
@@ -18,15 +15,38 @@ export default async function Home({ params }: { params: { id: string } }) {
         <h1 className="text-3xl sm:text-4xl my-5">調査の事前説明</h1>
         <h4>【質問の概要】</h4>
         <div className="textStyle">
-          <SplitText text={G_text1} />
+          <p>1000円を2人で分けるゲームを行います。</p>
+          <p className="mb-3">
+            2人の参加者は「提案者」と「応答者」に分けられ、以下のルールのゲームを進行します。
+          </p>
+          <ol className="list-decimal list-inside">
+            <li>提案者が応答者に対し1000円の分配額を提示する</li>
+            <li>
+              応答者は提示された分配に対し,受け入れるか断るかを選択する．
+            </li>
+            <li>
+              応答者が掲示を受け入れた場合はその金額による分配が行われ，
+              <span className="font-bold">断った場合は提案者と応答者は共にお金を受け取ることはできない．</span>
+            </li>
+          </ol>
         </div>
         <h4>【質問について】</h4>
         <div className="textStyle">
-          <SplitText text={G_text2} />
+          <p>
+            上記の問題設定のもとゲームを複数回行います．質問文の説明に則って，提案者の場合は分配金額を半角数字入力し，
+            応答者の場合は提示された分配に応じるかを選択してください．各ゲームにおいて，同じ相手,または違う相手と分配を行う可能性があります．
+          </p>
         </div>
-        <h4>【回答上の注意】</h4>
+        <h4>【回答にあたって】</h4>
         <div className="textStyle">
-          <SplitText text={G_text3} />
+          <p>
+            一度送信した内容はその後、修正することはできませんのでご注意ください。
+            また、実験開始から終了までの間、ブラウザの戻るボタンはご使用にならないでください。
+            データが正常に送信・処理されない可能性があります。
+            また、本調査は完全にオンライン上で行われており、参加者の匿名性は確保されています。
+            提案者に選ばれた場合は相手にあげる金額を、半角数字で入力してください。
+            応答者に選ばれた場合は画面に表示された提案を受け入れるか断るかを選んでください。
+          </p>
         </div>
         <div className="textStyle">
           それでは、まずは以下のボタンから回答される方のご年齢・性別・現在住んでいる都道府県を入力してください。
--- src/components/AgreementForm.tsx
+++ src/components/AgreementForm.tsx
@@ -2,8 +2,6 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { AgreementFormDataType } from "@/lib/formDataTypes";
-import SplitText from "./SplitText";
-import { D_text1, D_text2, D_text3, D_text4 } from "@/lib/textlist";
 
 export default function AgreementForm({ id }: { id: string }) {
   const router = useRouter();
@@ -34,7 +32,6 @@ export default function AgreementForm({ id }: { id: string }) {
       example: example,
     };
 
-
     try {
       const response = await fetch("/api/secondagree", {
         method: "POST",
@@ -62,16 +59,30 @@ export default function AgreementForm({ id }: { id: string }) {
   return (
     <>
       <div className="textStyle">
-        <SplitText text={D_text1} />
+        <p>
+          ご協力いただきありがとうございました。本ページはアンケート調査の事後説明ページです。
+          改めて調査の目的を説明したのち、データの提供について同意するかをお答えいただければと思います。
+        </p>
       </div>
 
       <div className="textStyle">
         <h3>【調査の目的について】</h3>
-        <SplitText text={D_text2} />
+        <p>
+          調査の事前説明でも述べたとおり、本アンケートの目的はお金の分配に関する調査を行うことです。
+          ですが正確には初めに提示された金額がその後の分配行動にどのような影響を及ぼすかを調べることが本アンケートの主目的となります。
+          そのため、1回目の質問時には全ての参加者に応答役が割り当てられており、金額の異なる2通りの分配額が提示されていました。
+          また、2回目の質問ではその後の分配行動を調査するため、参加者全員が提案者役に割り当てられています。
+          従って、実験の事前説明のうち「役割の振り分けは各ゲーム時にランダムに行われる」という点は実際とは異なる説明でした。
+          1回目のゲームで提示される金額は250円と500円、750円の2通りしかなく、調査実施者側で設定したものとなっております。
+          こちらも実験の事前説明で説明したものとは異なっております。
+        </p>
       </div>
 
       <div className="textStyle">
-        <SplitText text={D_text3} />
+        <p>
+          提示された金額の違いがその後の分配行動にどう影響するか、回答者の自然な反応を調査するために、
+          事実はと異なる説明を行いました。ご理解いただければ幸いです。
+        </p>
       </div>
 
       <div className="textStyle">
@@ -90,7 +101,14 @@ export default function AgreementForm({ id }: { id: string }) {
 
       <h3>【データ提供の可否について】</h3>
       <div className="textStyle">
-        <SplitText text={D_text4} />
+        <p>
+          以上の内容を踏まえた上で、データの提供について同意いただけるのであれば、下のチェックボックスをクリックし、
+          「確認番号表示」ボタンを押してください。ページを移動したのち、確認番号が画面に表示されます。
+          また、データの提供について同意いただけない際は、直接このページのタブを閉じていただいて構いません。
+          その場合、すでにアンケートに回答いただいている場合でも謝礼をお渡しすることはできず、お答えいただいた内容は削除されます。
+          ご理解いただければ幸いです。
+          改めまして、この度は調査にご参加いただきありがとうございました。
+        </p>
       </div>
 
       <div>
