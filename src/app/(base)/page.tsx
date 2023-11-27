d09ecea497c61a3b22b85733e20787f15d0549e2
HyuDev7 <hyu.df7@gmail.com>
2023-11-27 13:18:14 +0900

    test for event listener

--- src/app/(base)/page.tsx
+++ src/app/(base)/page.tsx
@@ -1,13 +1,31 @@
+"use client";
 import Link from "next/link";
 import SplitText from "@/components/SplitText";
 import { T_text1, T_text2 } from "@/lib/textlist";
+import { useEffect,useState } from "react";
+import { useLeavePageConfirmation } from "@/components/useLeavePageConfirmation";
+import { usePathname } from "next/navigation";
+
 
 export default function Page() {
+  useEffect(() => {
+    console.dir(typeof window.onload);
+
+    window.addEventListener("beforeunload", (e: BeforeUnloadEvent) => {
+      e.preventDefault();
+      e.returnValue="";
+      alert("hi!")
+      return;
+    });
+  }, []);
+
 
   return (
-    <main>
+    <main id="main">
       <div className="container mx-auto">
-        <h1 className="text-3xl sm:text-4xl my-5">分配の考え方に関するアンケート調査</h1>
+        <h1 className="text-3xl sm:text-4xl my-5">
+          分配の考え方に関するアンケート調査
+        </h1>
         <div className="textStyle">
           <SplitText text={T_text1} />
         </div>
